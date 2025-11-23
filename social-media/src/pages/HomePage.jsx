c02ff6d7ab9ebec9b16d63465aafe4adca285c82
elsaferati <elsavferati@gmail.com>
2025-11-23 09:20:42 +0100

    updates home page

--- social-media/src/pages/HomePage.jsx
+++ social-media/src/pages/HomePage.jsx
@@ -1,6 +1,4 @@
 import React, { useState } from "react";
-import Header from "../components/Header";
-import Sidebar from "../components/Sidebar";
 import PostsFeed from "../components/PostsFeed";
 import CreatePost from "../components/CreatePost";
 import ModalSystem from "../components/ModalSystem";
@@ -12,28 +10,16 @@ const HomePage = () => {
   const closeCreatePost = () => setIsCreatePostOpen(false);
 
   return (
-    <div className="home-page flex flex-col h-screen">
-      {/* Header */}
-      <Header />
+    <div className="home-page">
+      <button
+        onClick={openCreatePost}
+        className="bg-blue-500 text-white px-4 py-2 rounded mb-4"
+      >
+        Create Post
+      </button>
 
-      <div className="content flex flex-1">
-        {/* Sidebar */}
-        <Sidebar />
+      <PostsFeed />
 
-        {/* Main Feed */}
-        <main className="flex-1 p-4">
-          <button
-            onClick={openCreatePost}
-            className="bg-blue-500 text-white px-4 py-2 rounded mb-4"
-          >
-            Create Post
-          </button>
-
-          <PostsFeed />
-        </main>
-      </div>
-
-      {/* Modal for creating post */}
       {isCreatePostOpen && (
         <ModalSystem onClose={closeCreatePost}>
           <CreatePost onClose={closeCreatePost} />
